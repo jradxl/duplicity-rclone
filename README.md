de04c492ee8f4a84d4f385fdfdff2fabf57d5a82
John R <jradxl@gmail.com>
2020-01-10 12:55:51 +0000

    Updating to Python3

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# duplicity-rclone
+# duplicity-rclone for Python3
 [Duplicity](http://duplicity.nongnu.org/) backend using [rclone](http://rclone.org/)
 
 Rclone is a powerful command line program to sync files and directories to and from various cloud storage providers.
@@ -10,8 +10,12 @@ Since I've never wrote Python before today, use it at your own risk. Every feedb
 
 **Update 19/05/2017:** Since Amazon banned rclone, I moved my data to Google Drive, and it works smoothly as expected.
 
+**Update 2010/01/10:** Updated to run using Python3.
+
+
 # Setup
 Install `rclonebackend.py` into duplicity backend directory. This backend provides support for prefix `rclone://`.
+Duplicity Backend directory is similar to: /...../duplicity/backends
 
 # Usage
 Once you have configured rclone and successfully set up a remote (e.g. `gdrive` for Google Drive), assuming you can list your remote files with
@@ -23,3 +27,22 @@ you can start your backup with
 duplicity /mydocuments rclone://gdrive:/mydocuments
 ```
 **Please note the slash after the second colon.** Some storage provider will work with or without slash after colon, but some other will not. Since duplicity will complain about malformed URL if a slash is not present, **always put it after the colon**, and the backend will handle it for you.
+
+# Development setup (John Radley)
+Windows 10 Host, VirtualBox 6.1.0, Ubuntu 19.10
+Installed Brew, from https://docs.brew.sh/Homebrew-on-Linux
+Installed Pipenv, from https://pipenv-fork.readthedocs.io/en/latest
+  using: $brew install pipenv
+Installed Pycharm Community Edition (excellent!)
+--as the honesty of the original author, my Python skills are low.
+-- it took longer to setup development environment than fix the software!!
+
+# Test commands (so far!), for Pycharm's parameters field. First to see how code runs!
+full -v 9 --log-file=/home/john/duplicitytest-dest/log.log /home/john/duplicitytest-src/ file:///home/jradley/duplicitytest-dest
+full -v 9 --log-file=/home/john/duplicitytest-dest/log.log /home/john/duplicitytest-src/ rclone://Box1:/test001
+remove-older-than now --force -v 9 --log-file=/home/john/duplicitytest-dest/log.log  rclone://Box1:/test001
+
+
+//End
+10th Jan 2020
+
--- rclonebackend.py
+++ rclonebackend.py
@@ -1,90 +1,100 @@
 """
-Duplicity backend using rclone
+Duplicity backend using rclone, for Python3
 Rclone is a powerful command line program to sync files and directories to and from various cloud storage providers.
 
 File name: rclonebackend.py
 Author: Francesco Magno
+Updated for Python3: John Radley
 Date created: 10/12/2016
+Date updated: 2020-01-10
 Licence: GPL-3.0
-Repository: https://github.com/GilGalaad/duplicity-rclone
-Python Version: 2.7
+Original Repository: https://github.com/GilGalaad/duplicity-rclone
+Updated Version Repository: https://github.com/jradxl/duplicity-rclone
+Python Version: 3.7
 """
 
 import os
 import os.path
 
 import duplicity.backend
-from duplicity import path
 from duplicity import log
 from duplicity.errors import BackendException
+
 
 class RcloneBackend(duplicity.backend.Backend):
 
 	def __init__(self, parsed_url):
 		duplicity.backend.Backend.__init__(self, parsed_url)
-		self.rclone_cmd = "rclone"
+		self.rclone_cmd = u"rclone"
 		self.parsed_url = parsed_url
 		self.remote_path = self.parsed_url.path
-
 		try:
-			rc, o, e = self._subprocess(self.rclone_cmd + " --version")
+			rc, o, e = self._subprocess(self.rclone_cmd + u" --version")
 		except Exception:
-			log.FatalError("rclone not found: please install rclone", log.ErrorCode.backend_error)
+			log.FatalError(u"rclone not found: please install rclone", log.ErrorCode.backend_error)
 
-		if parsed_url.path.startswith("//"):
-			self.remote_path = self.remote_path[2:].replace(":/",":",1)
+		if parsed_url.path.startswith(u"//"):
+			self.remote_path = self.remote_path[2:].replace(u":/", u":", 1)
 
 	def _get(self, remote_filename, local_path):
-		temp_dir = os.path.dirname(local_path.name)
-		commandline = "%s copy %s/%s %s" % (self.rclone_cmd, self.remote_path, remote_filename, temp_dir)
+		sremf = remote_filename.decode(u"utf-8", errors=u"ignore")
+		slocp = local_path.uc_name
+		temp_dir = os.path.dirname(slocp)
+		commandline = u"%s copy %s/%s %s" % (self.rclone_cmd, self.remote_path, sremf, temp_dir)
 		rc, o, e = self._subprocess(commandline)
 		if rc != 0:
-			if os.path.isfile(os.path.join(temp_dir, remote_filename)):
-				os.remove(os.path.join(temp_dir, remote_filename))
-			raise BackendException(e.split('\n')[0])
-		os.rename(os.path.join(temp_dir, remote_filename), local_path.name)
+			if os.path.isfile(os.path.join(temp_dir, sremf)):
+				os.remove(os.path.join(temp_dir, sremf))
+			raise BackendException(e.split(b'\n')[0])
+		os.rename(os.path.join(temp_dir, sremf), slocp)
 
 	def _put(self, source_path, remote_filename):
-		temp_dir = os.path.dirname(source_path.name)
-		temp_filename = os.path.basename(source_path.name)
-		os.rename(source_path.name, os.path.join(temp_dir, remote_filename))
-		commandline = "%s copy --include %s %s %s" % (self.rclone_cmd, remote_filename, temp_dir, self.remote_path)
+		ssrcp = source_path.uc_name
+		sremf = remote_filename.decode("utf-8", errors="ignore")
+		sremp = self.remote_path
+		temp_dir = os.path.dirname(ssrcp)
+		temp_filename = os.path.basename(ssrcp)
+		os.rename(ssrcp, os.path.join(temp_dir, sremf))
+		commandline = u"%s copy --include %s %s %s" % (self.rclone_cmd, sremf, temp_dir, sremp)
 		rc, o, e = self._subprocess(commandline)
 		if rc != 0:
-			os.rename(os.path.join(temp_dir, remote_filename), source_path.name)
-			raise BackendException(e.split('\n')[0])
-		os.rename(os.path.join(temp_dir, remote_filename), source_path.name)
+			os.rename(os.path.join(temp_dir, sremf), ssrcp)
+			raise BackendException(e.split(b'\n')[0])
+		os.rename(os.path.join(temp_dir, sremf), ssrcp)
 
 	def _list(self):
 		filelist = []
-		commandline = "%s ls %s" % (self.rclone_cmd, self.remote_path)
+		commandline = u"%s ls %s" % (self.rclone_cmd, self.remote_path)
 		rc, o, e = self._subprocess(commandline)
 		if rc != 0:
-			if e.endswith("not found\n"):
+			if e.endswith(b"not found\n"):
 				return filelist
 			else:
-				raise BackendException(e.split('\n')[0])
+				raise BackendException(e.split(b'\n')[0])
 		if not o:
 			return filelist
-		lines = o.split('\n')
+		lines = o.split(b'\n')
 		for x in lines:
 			if x:
 				filelist.append(x.split()[-1])
 		return filelist
 
 	def _delete(self, remote_filename):
-		commandline = "%s delete --drive-use-trash=false --include %s %s" % (self.rclone_cmd, remote_filename, self.remote_path)
+		sremf = remote_filename.decode("utf-8", errors="ignore")
+		commandline = u"%s delete --drive-use-trash=false --include %s %s" % (self.rclone_cmd, sremf, self.remote_path)
 		rc, o, e = self._subprocess(commandline)
 		if rc != 0:
-			raise BackendException(e.split('\n')[0])
+			raise BackendException(e.split(b'\n')[0])
 
 	def _subprocess(self, commandline):
 		import shlex
 		from subprocess import Popen, PIPE
-		log.Info("Executing subprocess: '%s'" % commandline)
+		log.Info(u"Executing subprocess: '%s'" % commandline)
 		args = shlex.split(commandline)
 		p = Popen(args, stdout=PIPE, stderr=PIPE)
 		stdout, stderr = p.communicate()
 		return p.returncode, stdout, stderr
 
-duplicity.backend.register_backend("rclone", RcloneBackend)
+
+duplicity.backend.register_backend(u"rclone", RcloneBackend)
+
